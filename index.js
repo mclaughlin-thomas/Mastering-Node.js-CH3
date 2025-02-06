2930f38d2a3978a8e9a29d3aa32fe02da0c9cda5
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-06 11:12:49 -0500

    fixed index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function sum(first, second){
 }
 
 let result = sum(10,10);
-console.log('Result value: ${result}, Result type: ${typeof result}');
+console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
 result = sum(10, "10");
-console.log('Result value: ${result}, Result type: ${typeof result}');
+console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
