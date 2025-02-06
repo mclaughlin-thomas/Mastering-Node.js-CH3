66693621947d3e1326d700b6cb137392206e5c02
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-06 11:09:11 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, World");
+function sum(){
+    return first+second;
+}
+
+let result = sum(10,10);
+console.log('Result value: ${result}, Result type: ${typeof result}');
+result = sum(10, "10");
+console.log('Result value: ${result}, Result type: ${typeof result}');
