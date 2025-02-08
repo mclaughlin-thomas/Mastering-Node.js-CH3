4aa3e7c2e82501e0fa551595ab449e120c46faee
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-07 21:23:54 -0500

    typecheck

---
+++ index.ts
@@ -0,0 +1,8 @@
+function sum(first: number, second: number){
+    return first + second;
+}
+
+let result = sum(10,10);
+console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
+result = sum(10, "10");
+console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
