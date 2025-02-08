50679b133bc528b718309e5a1836c805fcfc2818
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-07 21:44:23 -0500

    inline template string

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//js primative types: string number boolean undefined and null
+//template strings allow data values to be specified in line with static content
+// ' not same as `
+let place2;
+console.log(`Place Value: ${place2} Type: ${typeof (place2)}`);
+//-----------
+//run with npm run use_ts
+// let defines variables
+// // const for constants...
+// Weakly typed
+// let condition = true;
+// let person = "bob";
+// const age = 40;
+// More informed
+// let condition1: boolean = true;
+// let person1: string = "bob";
+// const age1: number = 40;
+// let place = null;
+// null value is object
+// ----------------
 // function sum(first: number, second: number | string){
 //     if (typeof second == "string"){
 //         return first+Number.parseInt(second);
@@ -7,13 +28,6 @@
 //         return first+second;
 //     }
 // }
-Object.defineProperty(exports, "__esModule", { value: true });
-//run with npm run use_ts
-// let defines variables
-// // const for constants...
-let condition = true;
-let person = "bob";
-const age = 40;
 // let result = sum(10,10);
 // console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
 // result = sum(10, "10");
--- index.ts
+++ index.ts
@@ -1,3 +1,31 @@
+
+//js primative types: string number boolean undefined and null
+//template strings allow data values to be specified in line with static content
+// ' not same as `
+let place2: string | undefined | null;
+console.log(`Place Value: ${place2} Type: ${typeof(place2)}`)
+
+
+
+
+//-----------
+//run with npm run use_ts
+// let defines variables
+// // const for constants...
+// Weakly typed
+// let condition = true;
+// let person = "bob";
+// const age = 40;
+
+// More informed
+// let condition1: boolean = true;
+// let person1: string = "bob";
+// const age1: number = 40;
+
+// let place = null;
+// null value is object
+
+// ----------------
 // function sum(first: number, second: number | string){
 //     if (typeof second == "string"){
 //         return first+Number.parseInt(second);
@@ -6,19 +34,6 @@
 //         return first+second;
 //     }
 // }
-
-//run with npm run use_ts
-// let defines variables
-// // const for constants...
-// Weakly typed
-let condition = true;
-let person = "bob";
-const age = 40;
-
-// More informed
-let condition1: boolean = true;
-let person1: string = "bob";
-const age1: number = 40;
 
 // let result = sum(10,10);
 // console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
