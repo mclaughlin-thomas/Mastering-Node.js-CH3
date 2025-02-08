ab8709583615187f81f5432778ae5c6a7495d84e
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-07 21:31:41 -0500

    added notes

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 function sum(first: number, second: number | string){
-    // if (typeof second == "string"){
-    //     return first+Number.parseInt(second);
-    // }
-    // else{
-    return first+(second as any);
-    // }
+    if (typeof second == "string"){
+        return first+Number.parseInt(second);
+    }
+    else{
+        return first+second;
+    }
 }
 
+//run with npm run use_ts
 let result = sum(10,10);
 console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
 result = sum(10, "10");
