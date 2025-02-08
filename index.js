e33359e3e31f3ac3168e5a763f814d0bca9cd68e
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-07 21:35:47 -0500

    variable assignments

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 "use strict";
+// function sum(first: number, second: number | string){
+//     if (typeof second == "string"){
+//         return first+Number.parseInt(second);
+//     }
+//     else{
+//         return first+second;
+//     }
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-function sum(first, second) {
-    if (typeof second == "string") {
-        return first + Number.parseInt(second);
-    }
-    else {
-        return first + second;
-    }
-}
-let result = sum(10, 10);
-console.log('Result value: ' + result + ' , Result type: ' + (typeof result));
-result = sum(10, "10");
-console.log('Result value: ' + result + ' , Result type: ' + (typeof result));
+//run with npm run use_ts
+// let defines variables
+// // const for constants...
+let condition = true;
+let person = "bob";
+const age = 40;
+// let result = sum(10,10);
+// console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
+// result = sum(10, "10");
+// console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
-function sum(first: number, second: number | string){
-    if (typeof second == "string"){
-        return first+Number.parseInt(second);
-    }
-    else{
-        return first+second;
-    }
-}
+// function sum(first: number, second: number | string){
+//     if (typeof second == "string"){
+//         return first+Number.parseInt(second);
+//     }
+//     else{
+//         return first+second;
+//     }
+// }
 
 //run with npm run use_ts
-let result = sum(10,10);
-console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
-result = sum(10, "10");
-console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
+// let defines variables
+// // const for constants...
+// Weakly typed
+let condition = true;
+let person = "bob";
+const age = 40;
+
+// More informed
+let condition1: boolean = true;
+let person1: string = "bob";
+const age1: number = 40;
+
+// let result = sum(10,10);
+// console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
+// result = sum(10, "10");
+// console.log('Result value: ' + result+ ' , Result type: ' + (typeof result));
