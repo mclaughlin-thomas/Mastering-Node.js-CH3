a37b09f09153ba18ea6977a7b684d0747e5c1469
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-07 21:31:01 -0500

    ambiguity

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 function sum(first: number, second: number | string){
-    if (typeof second == "string"){
-        return first+Number.parseInt(second);
-    }
-    else{
-        return first+second;
-    }
+    // if (typeof second == "string"){
+    //     return first+Number.parseInt(second);
+    // }
+    // else{
+    return first+(second as any);
+    // }
 }
 
 let result = sum(10,10);
