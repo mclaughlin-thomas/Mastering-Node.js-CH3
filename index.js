55a148968cf54316a6436b001a7d34abd45f21bd
thomas <thomas.mclaughlin2@stvincent.edu>
2025-02-10 22:16:51 -0500

    working with objects

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//Working with objects
+let hat = {
+    name: "Hat",
+    price: 100
+};
+let boots = {
+    name: "Boots",
+    price: 100
+};
+console.log(`Name: ${hat.name}, Price: ${hat.price}`);
+console.log(`Name: ${boots.name}, Price: ${boots.price}`);
+// ---------------
 //defining and using functions
-function writeValue(val) {
-    console.log(`Value: ${val ?? "Fallback value"}`);
-}
-writeValue("London");
-writeValue(null);
+// function writeValue(val: string | null) {
+//     console.log(`Value: ${val ?? "Fallback value"}`)
+// }
+// writeValue("London");
+// writeValue(null);
 //functions are defined with the function keyword and are given a name
 // write value deines a parameter it will accept string or null values
 // by default ts will allow funcitons to be invoked only when the # of args matches the number of paramters the function defines
 // in js though a function can be called w any number of arguments
 //Optional paramter. The ? is used to denote an optional paramter
-function writeValue2(val) {
-    console.log(`Value: ${val ?? "Fallback value"}`);
-}
-writeValue2("London");
-writeValue2();
+// function writeValue2(val?: string) {
+//     console.log(`Value: ${val ?? "Fallback value"}`)
+// }
+// writeValue2("London");
+// writeValue2();
 // Here the funciton can be invoked with zero or one argument
 // Here the parameter type is string or undefined  (string | undefined) because the value will be undefined
 // if the function is invoked w/o an argument.
@@ -24,117 +36,120 @@ writeValue2();
 // default values can be helpful when dealing with undefined values
 //default value will be used when fcn is invoked w/o an argument
 // this means that the type of the param in the example will always be string, so I dont have to check for undefined values
-function writeValue3(val = "default value") {
-    console.log(`Value: ${val}`);
-}
-writeValue3("London");
-writeValue3();
+// function writeValue3(val: string = "default value"){
+//     console.log(`Value: ${val}`)
+// }
+// writeValue3("London");
+// writeValue3();
 // defining rest parameters
 // rest parameters are used to capture any additional arguments when
 // a fcn is invoked w additional arguments
 //rest parameter must be the last parameter defined by fcn.
 //its name must be prefixed with an ellipsis (...)
 // the rest parameter is an array to which any extra arguments will be assigned
-function writeValue4(val, ...extraInfo) {
-    console.log(`Value: ${val}, Extras: ${extraInfo}`);
-}
-writeValue4("London", "Raining", "Cold");
-writeValue4("Paris", "Sunny");
-writeValue4("New York");
-// -----------
-console.log("Return");
-function composeString(val) {
-    return `Composed string: ${val}`;
-}
-function writeValue5(val) {
-    console.log(composeString(val ?? "Fallback value"));
-}
-writeValue5("London");
-writeValue5();
+// function writeValue4(val: string, ...extraInfo: string[]) {
+//     console.log(`Value: ${val}, Extras: ${extraInfo}`)
+// }
+// writeValue4("London", "Raining", "Cold");
+// writeValue4("Paris", "Sunny");
+// writeValue4("New York");
+// // -----------
+// console.log("Return")
+// function composeString(val: string) : string {
+//     return `Composed string: ${val}`;
+// }
+// function writeValue5(val?: string) {
+//     console.log(composeString(val ?? "Fallback value"));
+// }
+// writeValue5("London");
+// writeValue5();
 // -------------
-function getUKCapital() {
-    return "London";
-}
-function writeCity(f) {
-    // the string that it writes out.
-    console.log(`City: ${f()}`);
-}
-writeCity(getUKCapital);
-// ---------------
-function getUKCapital2() {
-    return "London";
-}
-function writeCity2(f) {
-    console.log(`City: ${f()}`);
-}
-writeCity2(getUKCapital2);
-writeCity2(() => "Paris");
+// function getUKCapital() : string {
+//     return "London";
+// }
+// function writeCity(f:() => string){ // The writeCity function defines a parameter called f , which is a function that it invokes to get the value to insert into 
+//                                     // the string that it writes out.
+//     console.log(`City: ${f()}`)
+// }
+// writeCity(getUKCapital);
+// // ---------------
+// function getUKCapital2() : string {
+//     return "London";
+// }
+// function writeCity2(f:() => string){ 
+//     console.log(`City: ${f()}`)
+// }
+// writeCity2(getUKCapital2);
+// writeCity2(() => "Paris");
 // -------------
 //Functions can access values that are defined in the surrounding code, using a
 //feature called closure, as demonstrated in Listing 3.41.
-console.log("closure");
-function getUKCapital3() {
-    return "London";
-}
-function writeCity3(f) {
-    console.log(`City: ${f()}`);
-}
-writeCity3(getUKCapital3);
-writeCity3(() => "Paris");
-let myCity = "Rome";
-writeCity3(() => myCity);
+// console.log ("closure")
+// function getUKCapital3() : string {
+//     return "London";
+// }
+// function writeCity3(f: () => string){
+//     console.log(`City: ${f()}`)
+// }
+// writeCity3(getUKCapital3);
+// writeCity3(() => "Paris");
+// let myCity = "Rome";
+// writeCity3(() => myCity);
 // -----------
-let myArray = []; // js arrays will resize themselves
+//let myArray = []; // js arrays will resize themselves
 // dont have to declare data type it will hold either. Below we have a number, string and a boolean
-myArray[0] = 100;
-myArray[1] = "Adam";
-myArray[2] = true;
-// -- similar
-let myArray2 = [];
-myArray2[0] = 100;
-myArray2[1] = "Adam";
-myArray2[2] = true;
+// myArray[0] = 100;
+// myArray[1] = "Adam";
+// myArray[2] = true;
+// // -- similar
+// let myArray2: any[] = [];
+// myArray2[0] = 100;
+// myArray2[1] = "Adam";
+// myArray2[2] = true;
 // -------------
 // restricting array value types
-let myArray3 = [];
-myArray3[0] = 100;
-myArray3[1] = "Adam";
-myArray3[2] = true;
+// let myArray3: (number | string | boolean)[] = [];
+// myArray3[0] = 100;
+// myArray3[1] = "Adam";
+// myArray3[2] = true;
 // one-liner to do same thing
-let myArray4 = [100, "Adam", true];
-// ------------
-let myArray5 = [100, "Adam", true];
-let val = myArray5[0];
-console.log(`Value: ${val}`);
+// let myArray4: (number | string | boolean)[] = [100,"Adam", true];
+// // ------------
+// let myArray5: (number | string | boolean)[] = [100,"Adam", true];
+// let val = myArray5[0];
+// console.log(`Value: ${val}`);
 // ------------
 // modigying contents of array
-let myArray6 = [100, "Adam", true];
-myArray6[0] = "Tuesday";
-let val3 = myArray[0];
-console.log(`Value: ${val3}`);
+// let myArray6: (number | string | boolean)[] = [100,"Adam", true];
+// myArray6[0] = "Tuesday";
+// let val3 = myArray[0];
+// console.log(`Value: ${val3}`);
 // ---------
 //enumerating contents of an arrray. Enumerate through contents of array using for loop or
 // the forEach loop.
-console.log("enumeration");
-let myArray7 = [100, "Adam", true];
-for (let i = 0; i < myArray7.length; i++) {
-    console.log("Index " + i + ": " + myArray7[i]);
-}
-console.log("XXXX");
-myArray7.forEach((value, index) => console.log("Index " + index + ": " + value));
-// -----------
-console.log("Spread Operator");
+// console.log ("enumeration")
+// let myArray7: (number | string | boolean)[] = [100,"Adam", true];
+// for (let i = 0; i < myArray7.length; i++) {
+//     console.log("Index " + i + ": " + myArray7[i]);
+// }
+// console.log("XXXX");
+// myArray7.forEach((value, index) =>
+//     console.log("Index " + index + ": " + value));
+// // -----------
+// console.log("Spread Operator")
 //spread operator
 // used to expand an array so contents can be used as function arguments or comvined w other arrays
-let myArray10 = [100, "Adam", true];
-let otherArray = [...myArray10, 200, "Bob", false];
+// let myArray10: (number | string | boolean)[] = [100,"Adam", true];
+// let otherArray = [...myArray10, 200, "Bob", false];
 // spread operator is an ellipsis and it causes the arry to be unpacked
 // here, otherArray has all contents of myArray and then 200, bob and false
 // for (let i = 0; i < myArray.length; i++) {
 //console.log("Index " + i + ": " + myArray10[i]);
 // }
 // console.log("---");
-otherArray.forEach((value, index) => console.log("Index " + index + ": " + value));
+// otherArray.forEach((value, index) =>
+//     console.log("Index " + index + ": " + value));
+// -----------------------------------------------------
 // () = >
 // this is called arrow syntax or fat arrow syntax or lambda expression syntax
 // the thing the arrow points to the the function result
